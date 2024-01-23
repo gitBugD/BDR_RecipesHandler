b1104dedc0b3570082eba3b8f1097245eae53e3b
Maxime-sch <55839375+Maxime-sch@users.noreply.github.com>
2024-01-23 16:30:48 +0100

    Fix insert_data.sql script

--- scripts/insert_data.sql
+++ scripts/insert_data.sql
@@ -1,32 +1,32 @@
-WITH allergen_nuts AS{
+WITH allergen_nuts AS (
 INSERT
 	INTO
 	public.allergen ("name")
 VALUES
 	 ('nuts') RETURNING id
-},
-allergen_eggs AS{
+),
+allergen_eggs AS(
 INSERT
 	INTO
 	public.allergen ("name")
 VALUES
 	 ('eggs') RETURNING id
-},
-allergen_lactose AS{
+),
+allergen_lactose AS(
 INSERT
 	INTO
 	public.allergen ("name")
 VALUES
 	 ('lactose') RETURNING id
-},
-allergen_fish AS{
+),
+allergen_fish AS(
 INSERT
 	INTO
 	public.allergen ("name")
 VALUES
 	 ('fish') RETURNING id
-},
-other_allergens AS{
+),
+other_allergens AS(
 INSERT
 	INTO
 	public.allergen ("name")
@@ -42,15 +42,15 @@ VALUES
 	 ('clams'),
 	 ('kiwi'),
 	 ('apple')
-},
-creator_remy AS{
+),
+creator_remy AS(
 INSERT
 	INTO
 	public.creator ("name")
 VALUES
 	 ('Remy le chef') RETURNING id
-},
-other_creators AS{
+),
+other_creators AS(
 INSERT
 	INTO
 	public.creator ("name")
@@ -58,7 +58,7 @@ VALUES
 	 ('Benedetta'),
 	 ('Pina'),
 	 ('Maxime')
-},
+),
 tool_grill_pan AS(
 INSERT
 	INTO
@@ -204,7 +204,7 @@ INSERT
 		allergen_eggs))
 RETURNING id
 ),
-ingredients_restrictions AS{
+ingredients_restrictions AS(
 INSERT
 	INTO
 	public.ingredient_restriction (idingredient,
@@ -245,8 +245,7 @@ SELECT
 	id
 FROM
 	ingredient_fish),
-'vegan');
-
+'vegan')), ingredients_seasons AS(
 INSERT
 	INTO
 	public.ingredient_season (idingredient,
@@ -281,28 +280,24 @@ SELECT
 	id
 FROM
 	ingredient_mozzarella),
-,
 'autumn'),
 	 ((
 SELECT
 	id
 FROM
 	ingredient_mozzarella),
-,
 'winter'),
 	 ((
 SELECT
 	id
 FROM
 	ingredient_mozzarella),
-,
 'summer'),
 	 ((
 SELECT
 	id
 FROM
 	ingredient_mozzarella),
-,
 'spring'),
 	 ((
 SELECT
@@ -520,8 +515,8 @@ SELECT
 FROM
 	ingredient_eggs),
 'autumn')
-},
-recipe_parmigiana AS{
+),
+recipe_parmigiana AS(
 INSERT
 	INTO
 	public.recipe ("name",
@@ -536,10 +531,10 @@ VALUES
 6,
 3,
 2,
-2),
+(SELECT id FROM creator_remy))
 RETURNING id
-} ,
-recipe_granola AS{
+) ,
+recipe_granola AS(
 INSERT
 	INTO
 	public.recipe ("name",
@@ -554,10 +549,10 @@ VALUES
 6,
 1,
 2,
-3),
+(SELECT id FROM creator_remy))
 	 RETURNING id
-	 },
-recipe_soup AS{INSERT
+	 ),
+recipe_soup AS(INSERT
 INTO
 	public.recipe ("name",
 	description,
@@ -571,73 +566,492 @@ VALUES
 4,
 1,
 2,
-4)
+(SELECT id FROM creator_remy))
 	RETURNING id
-	}, recipes_coursetypes AS{
-INSERT INTO public.recipe_coursetype (idrecipe,namecoursetype) VALUES
-	 ((SELECT id FROM recipe_parmigiana),'main course'),
-	 ((SELECT id FROM recipe_granola),'snack'),
-	 ((SELECT id FROM recipe_soup),'starter'),
-	 ((SELECT id FROM recipe_soup),'main course')},
-	 step_1_parmigiana AS{
-	 INSERT INTO public.step (idrecipe,nb,instructions,preptime,cookingtime) VALUES
-	 ((SELECT id FROM recipe_parmigiana),3,'In a baking dish, stratify eggplants alternated by tomato sauce, pieces of mozzarella and parmigiano. End with a generous amount of parmigiano.',10,NULL)
-	 RETURNING id},
-	 step_2_parmigiana AS{
-	 INSERT INTO public.step (idrecipe,nb,instructions,preptime,cookingtime) VALUES
-	 ((SELECT id FROM recipe_parmigiana),2,'Chop the mozzarella and grate the parmigiano.',10,NULL) 
-	RETURNING id
-	 },
-	 step_3_parmigiana AS{
-	 INSERT INTO public.step (idrecipe,nb,instructions,preptime,cookingtime) VALUES 
-	 ((SELECT id FROM recipe_parmigiana),1,'Grill the eggplants.',30,60) 
-	RETURNING id
-	 },
-	 step_4_parmigiana AS{
-	 INSERT INTO public.step (idrecipe,nb,instructions,preptime,cookingtime) VALUES
-	 ((SELECT id FROM recipe_parmigiana),4,'Cook in the oven (static mode) at 180 degrees. Last 10 minutes in grill mode.',0,50)
-	RETURNING id
-	 },
-	 step_1_granola AS{
-	 INSERT INTO public.step (idrecipe,nb,instructions,preptime,cookingtime) VALUES
-	 ((SELECT id FROM recipe_granola),1,'Smash the bananas with a fork.',5,0)
-	RETURNING id
-	 },
-	 step_2_granola AS{
-	 INSERT INTO public.step (idrecipe,nb,instructions,preptime,cookingtime) VALUES
-	 ((SELECT id FROM recipe_granola),2,'Mix it very well together with all other ingredients.',5,0)
-	RETURNING id
-	 },
-	 step_3_granola AS{
-	 INSERT INTO public.step (idrecipe,nb,instructions,preptime,cookingtime) VALUES
-	 ((SELECT id FROM recipe_granola),3,'Cook in the oven at 150 °C for about 1 hour and a half, turning every 30 minutes',0,90)
-	RETURNING id
-	 },
-	 step_1_soup AS{
-	 INSERT INTO public.step (idrecipe,nb,instructions,preptime,cookingtime) VALUES
-	 ((SELECT id FROM recipe_soup),1,'Cut the sweet potatoes and put them to boil together with the chestnuts already cooked, so their flavours can start to combine. Water should cover all the vegetables. Add some salt and spices like cinnamon and thymian, if you like.',20,0)
-	RETURNING id
-	 },
-	 step_2_soup AS{
-	 INSERT INTO public.step (idrecipe,nb,instructions,preptime,cookingtime) VALUES
-	 ((SELECT id FROM recipe_soup),2,'Cook for 30 minutes or until the potatoes are soft, then mix everything with a blender to make it creamy. If the consistency is too thick, feel free to add a bit more water. Enjoy! 😋',0,30)
-	 RETURNING id
-	 }, steps_ingredients AS{
-	 INSERT INTO public.step_ingredient (idrecipe,nbstep,idingredient,unit,quantity) VALUES
-	 ((SELECT id FROM recipe_parmigiana),(SELECT id FROM step_3_parmigiana),(SELECT id FROM ingredient_tomato_sauce),'gr',400.00),
-	 ((SELECT id FROM recipe_parmigiana),(SELECT id FROM step_2_parmigiana),(SELECT id FROM ingredient_parmigiano),'gr',300.00),
-	 ((SELECT id FROM recipe_parmigiana),(SELECT id FROM step_2_parmigiana),(SELECT id FROM ingredient_mozzarella),'gr',300.00),
-	 ((SELECT id FROM recipe_parmigiana),(SELECT id FROM step_1_parmigiana),(SELECT id FROM ingredient_eggplant),'u',4.00),
-	 ((SELECT id FROM recipe_granola),(SELECT id FROM step_1_granola),(SELECT id FROM ingredient_bananas),'u',2.00),
-	 ((SELECT id FROM recipe_granola),(SELECT id FROM step_2_granola),(SELECT id FROM ingredient_oats),'gr',300.00),
-	 ((SELECT id FROM recipe_granola),(SELECT id FROM step_2_granola),(SELECT id FROM ingredient_nuts),'gr',100.00),
-	 ((SELECT id FROM recipe_granola),(SELECT id FROM step_2_granola),(SELECT id FROM ingredient_sunflower_seeds),'gr',50.00),
-	 ((SELECT id FROM recipe_granola),(SELECT id FROM step_2_granola),(SELECT id FROM ingredient_seed_oil),'gr',40.00),
-	 ((SELECT id FROM recipe_soup),(SELECT id FROM step_1_soup),(SELECT id FROM ingredient_boiled_chestnuts),'gr',250.00),
-	 ((SELECT id FROM recipe_soup),(SELECT id FROM step_1_soup),(SELECT id FROM ingredient_sweet_potatoes),'gr',400.00)}	 
-INSERT INTO public.step_tool (idrecipe,nbstep,idtool) VALUES
-	 ((SELECT id FROM recipe_parmigiana),(SELECT id FROM step_4_parmigiana),(SELECT id FROM tool_oven)),
-	 ((SELECT id FROM recipe_parmigiana),(SELECT id FROM step_3_parmigiana),(SELECT id FROM tool_baking_dish)),
-	 ((SELECT id FROM recipe_parmigiana),(SELECT id FROM step_1_parmigiana),(SELECT id FROM tool_grill_pan)),
-	 ((SELECT id FROM recipe_granola),(SELECT id FROM step_3_granola),(SELECT id FROM tool_oven)),
-	 ((SELECT id FROM recipe_soup),(SELECT id FROM step_2_soup),(SELECT id FROM tool_blender));
+	),
+recipes_coursetypes AS(
+INSERT
+	INTO
+	public.recipe_coursetype (idrecipe,
+	namecoursetype)
+VALUES
+	 ((
+SELECT
+	id
+FROM
+	recipe_parmigiana),
+'main course'),
+	 ((
+SELECT
+	id
+FROM
+	recipe_granola),
+'snack'),
+	 ((
+SELECT
+	id
+FROM
+	recipe_soup),
+'starter'),
+	 ((
+SELECT
+	id
+FROM
+	recipe_soup),
+'main course')),
+	 step_1_parmigiana AS(
+	 INSERT
+	INTO
+	public.step (idrecipe,
+	nb,
+	instructions,
+	preptime,
+	cookingtime)
+VALUES
+	 ((
+SELECT
+	id
+FROM
+	recipe_parmigiana),
+1,
+'Grill the eggplants.',
+30,
+60)
+	RETURNING nb
+	 ),
+	 step_2_parmigiana AS(
+	 INSERT
+	INTO
+	public.step (idrecipe,
+	nb,
+	instructions,
+	preptime,
+	cookingtime)
+VALUES
+	 ((
+SELECT
+	id
+FROM
+	recipe_parmigiana),
+2,
+'Chop the mozzarella and grate the parmigiano.',
+10,
+NULL)
+	RETURNING nb
+	 ),
+	 step_3_parmigiana AS(
+	 INSERT
+	INTO
+	public.step (idrecipe,
+	nb,
+	instructions,
+	preptime,
+	cookingtime)
+VALUES
+	 ((
+SELECT
+	id
+FROM
+	recipe_parmigiana),
+3,
+'In a baking dish, stratify eggplants alternated by tomato sauce, pieces of mozzarella and parmigiano. End with a generous amount of parmigiano.',
+10,
+NULL)
+	 RETURNING nb),
+	 step_4_parmigiana AS(
+	 INSERT
+	INTO
+	public.step (idrecipe,
+	nb,
+	instructions,
+	preptime,
+	cookingtime)
+VALUES
+	 ((
+SELECT
+	id
+FROM
+	recipe_parmigiana),
+4,
+'Cook in the oven (static mode) at 180 degrees. Last 10 minutes in grill mode.',
+0,
+50)
+	RETURNING nb
+	 ),
+	 step_1_granola AS(
+	 INSERT
+	INTO
+	public.step (idrecipe,
+	nb,
+	instructions,
+	preptime,
+	cookingtime)
+VALUES
+	 ((
+SELECT
+	id
+FROM
+	recipe_granola),
+1,
+'Smash the bananas with a fork.',
+5,
+0)
+	RETURNING nb
+	 ),
+	 step_2_granola AS(
+	 INSERT
+	INTO
+	public.step (idrecipe,
+	nb,
+	instructions,
+	preptime,
+	cookingtime)
+VALUES
+	 ((
+SELECT
+	id
+FROM
+	recipe_granola),
+2,
+'Mix it very well together with all other ingredients.',
+5,
+0)
+	RETURNING nb
+	 ),
+	 step_3_granola AS(
+	 INSERT
+	INTO
+	public.step (idrecipe,
+	nb,
+	instructions,
+	preptime,
+	cookingtime)
+VALUES
+	 ((
+SELECT
+	id
+FROM
+	recipe_granola),
+3,
+'Cook in the oven at 150 °C for about 1 hour and a half, turning every 30 minutes',
+0,
+90)
+	RETURNING nb
+	 ),
+	 step_1_soup AS(
+	 INSERT
+	INTO
+	public.step (idrecipe,
+	nb,
+	instructions,
+	preptime,
+	cookingtime)
+VALUES
+	 ((
+SELECT
+	id
+FROM
+	recipe_soup),
+1,
+'Cut the sweet potatoes and put them to boil together with the chestnuts already cooked, so their flavours can start to combine. Water should cover all the vegetables. Add some salt and spices like cinnamon and thymian, if you like.',
+20,
+0)
+	RETURNING nb
+	 ),
+	 step_2_soup AS(
+	 INSERT
+	INTO
+	public.step (idrecipe,
+	nb,
+	instructions,
+	preptime,
+	cookingtime)
+VALUES
+	 ((
+SELECT
+	id
+FROM
+	recipe_soup),
+2,
+'Cook for 30 minutes or until the potatoes are soft, then mix everything with a blender to make it creamy. If the consistency is too thick, feel free to add a bit more water. Enjoy! 😋',
+0,
+30)
+	 RETURNING nb
+	 ),
+steps_ingredients AS(
+	 INSERT
+	INTO
+	public.step_ingredient (idrecipe,
+	nbstep,
+	idingredient,
+	unit,
+	quantity)
+VALUES
+	 ((
+SELECT
+	id
+FROM
+	recipe_parmigiana),
+(
+SELECT
+	nb
+FROM
+	step_3_parmigiana),
+(
+SELECT
+	id
+FROM
+	ingredient_tomato_sauce),
+'gr',
+400.00),
+	 ((
+SELECT
+	id
+FROM
+	recipe_parmigiana),
+(
+SELECT
+	nb
+FROM
+	step_2_parmigiana),
+(
+SELECT
+	id
+FROM
+	ingredient_parmigiano),
+'gr',
+300.00),
+	 ((
+SELECT
+	id
+FROM
+	recipe_parmigiana),
+(
+SELECT
+	nb
+FROM
+	step_2_parmigiana),
+(
+SELECT
+	id
+FROM
+	ingredient_mozzarella),
+'gr',
+300.00),
+	 ((
+SELECT
+	id
+FROM
+	recipe_parmigiana),
+(
+SELECT
+	nb
+FROM
+	step_1_parmigiana),
+(
+SELECT
+	id
+FROM
+	ingredient_eggplant),
+'u',
+4.00),
+	 ((
+SELECT
+	id
+FROM
+	recipe_granola),
+(
+SELECT
+	nb
+FROM
+	step_1_granola),
+(
+SELECT
+	id
+FROM
+	ingredient_bananas),
+'u',
+2.00),
+	 ((
+SELECT
+	id
+FROM
+	recipe_granola),
+(
+SELECT
+	nb
+FROM
+	step_2_granola),
+(
+SELECT
+	id
+FROM
+	ingredient_oats),
+'gr',
+300.00),
+	 ((
+SELECT
+	id
+FROM
+	recipe_granola),
+(
+SELECT
+	nb
+FROM
+	step_2_granola),
+(
+SELECT
+	id
+FROM
+	ingredient_nuts),
+'gr',
+100.00),
+	 ((
+SELECT
+	id
+FROM
+	recipe_granola),
+(
+SELECT
+	nb
+FROM
+	step_2_granola),
+(
+SELECT
+	id
+FROM
+	ingredient_sunflower_seeds),
+'gr',
+50.00),
+	 ((
+SELECT
+	id
+FROM
+	recipe_granola),
+(
+SELECT
+	nb
+FROM
+	step_2_granola),
+(
+SELECT
+	id
+FROM
+	ingredient_seed_oil),
+'gr',
+40.00),
+	 ((
+SELECT
+	id
+FROM
+	recipe_soup),
+(
+SELECT
+	nb
+FROM
+	step_1_soup),
+(
+SELECT
+	id
+FROM
+	ingredient_boiled_chestnuts),
+'gr',
+250.00),
+	 ((
+SELECT
+	id
+FROM
+	recipe_soup),
+(
+SELECT
+	nb
+FROM
+	step_1_soup),
+(
+SELECT
+	id
+FROM
+	ingredient_sweet_potatoes),
+'gr',
+400.00))
+INSERT
+	INTO
+	public.step_tool (idrecipe,
+	nbstep,
+	idtool)
+VALUES
+	 ((
+SELECT
+	id
+FROM
+	recipe_parmigiana),
+(
+SELECT
+	nb
+FROM
+	step_4_parmigiana),
+(
+SELECT
+	id
+FROM
+	tool_oven)),
+	 ((
+SELECT
+	id
+FROM
+	recipe_parmigiana),
+(
+SELECT
+	nb
+FROM
+	step_3_parmigiana),
+(
+SELECT
+	id
+FROM
+	tool_baking_dish)),
+	 ((
+SELECT
+	id
+FROM
+	recipe_parmigiana),
+(
+SELECT
+	nb
+FROM
+	step_1_parmigiana),
+(
+SELECT
+	id
+FROM
+	tool_grill_pan)),
+	 ((
+SELECT
+	id
+FROM
+	recipe_granola),
+(
+SELECT
+	nb
+FROM
+	step_3_granola),
+(
+SELECT
+	id
+FROM
+	tool_oven)),
+	 ((
+SELECT
+	id
+FROM
+	recipe_soup),
+(
+SELECT
+	nb
+FROM
+	step_2_soup),
+(
+SELECT
+	id
+FROM
+	tool_blender))
